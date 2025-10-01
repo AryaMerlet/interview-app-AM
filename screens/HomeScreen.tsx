08cc8f8b466a37cae234ada07ad7cda8bbce0d17
AryaMerlet <151627770+AryaMerlet@users.noreply.github.com>
2025-10-01 11:23:18 +0200

    Creation/import of component & new styling
    Creation of a component folder
    Creation of CustomButton component
    Import of new component to HomeScreen
    update to the const style for better counter visiblity

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import {StatusBar} from 'expo-status-bar';
 import {Button, StyleSheet, Text, View} from 'react-native';
+import CustomButton from '../components/CustomButton';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Button title="UP" onPress={() => {}} />
-      <Text>0</Text>
-      <Button title="DOWN" onPress={() => {}} />
+      <CustomButton title="UP" onPress={() => {}} />
+      <Text style={styles.counter}>0</Text>
+      <CustomButton title="DOWN" onPress={() => {}} />
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//StyleSheet.create = create a new stylesheet as an object, for performance optimization and better debugging
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  counter: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    margin: 10,
   },
 });
