3cbbaba4f49760ddea69f81f04e6391d5a0a08ca
AryaMerlet <151627770+AryaMerlet@users.noreply.github.com>
2025-10-02 11:27:57 +0200

    Migrate API from basic fetch to using Redux RTK
    Update index.ts to include the api reducer and middleware
    Create apiSlice to handle all the API request
    Create productSlice to implement the endpoints for the API request
    Update ProductScreen to use the new logic

--- store/index.ts
+++ store/index.ts
@@ -1,10 +1,16 @@
 import {configureStore} from '@reduxjs/toolkit';
 import counterReducer from './feature/counter/counterSlice';
+import {apiSlice} from './feature/api/apiSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    // Listens to actions (pending, fulfilled, rejected), updates cache, tracks query metadata
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: getDefaultMiddleware =>
+    // Intercepts query actions, makes fetch requests, dispatches success/failure actions and manages subscription and cache
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
